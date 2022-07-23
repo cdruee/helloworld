c1e8a4e919c73dbc8b835b1d9098cc269a51ea2a
Clemens Druee <druee@uni-trier.de>
2022-07-24 00:50:13 +0200

    test gihub worflow 21

--- helloworld/_version.py
+++ helloworld/_version.py
@@ -3,7 +3,7 @@
 __title__ = 'helloworld'
 __description__ = 'Python package CI dummy.'
 __url__ = ''
-__version__ = '0.0.6a16'
+__version__ = '0.0.6a17'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'BSD-3-Clause'
